7d42fff1fd08b053c54c98078081b7f1517df80a
Jay Roever <jay.roever@cardinalhealth.com>
2021-02-18 14:33:21 -0500

    Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {AbstractControl, FormBuilder, FormControl, FormGroup, ValidatorFn, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'form-validation';
+  myForm = new FormGroup({
+    min: new FormControl(),
+    max: new FormControl()
+  });
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.myForm = this.fb.group({
+      min: [null, Validators.required],
+      max: [null, Validators.max(100)]
+    }, { validators: this.RangeValidator, updateOn: 'submit' });
+    this.myForm.controls.max.setValue(10);
+  }
+
+  RangeValidator: ValidatorFn = (fg: AbstractControl) => {
+    const min = fg.get('min')?.value;
+    const max = fg.get('max')?.value;
+
+    if (!min || !max) {
+      return null;
+    }
+    if (min > max) {
+      fg.get('min')?.setErrors({minGreatMax: true});
+      fg.get('max')?.setErrors({minGreatMax: true});
+    }
+    return null;
+  }
+
+  getErrorMessage(controlName: string): string | null {
+    if (this.myForm.get(controlName)?.hasError('required')) {return  'Required'; }
+    if (this.myForm.get(controlName)?.hasError('max')) {return  'Max is greater than 100'; }
+    if (this.myForm.get(controlName)?.hasError('minGreatMax')) {return 'Min must be greater than the Max'; }
+    return 'Undefined Error';
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import {ReactiveFormsModule} from '@angular/forms';
+import {NgBootstrapFormValidationModule} from 'ng-bootstrap-form-validation';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+    imports: [
+      BrowserModule,
+      AppRoutingModule,
+      NgbModule,
+      ReactiveFormsModule,
+      NgBootstrapFormValidationModule.forRoot(),
+      NgBootstrapFormValidationModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
